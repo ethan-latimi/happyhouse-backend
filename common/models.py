1d8ee168cfdb9cdc0e6ec0382430f4e7dec16f64
ethan <ethanminsu8969@gmail.com>
2023-06-20 16:46:10 +0900

    common model defined

--- common/models.py
+++ common/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# 재사용성을 위한 common model *model 상속과 class Meta -> abstract = True가 핵심 요소
+
+class CommonModel(models.Model):
+
+    """Common Model Definition"""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
