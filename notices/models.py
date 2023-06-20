64efb4a4f89036b8d489159cdb8af32fab601be7
ethan <ethanminsu8969@gmail.com>
2023-06-20 17:18:01 +0900

    reservations model defined

--- notices/models.py
+++ notices/models.py
@@ -4,14 +4,30 @@ from common.models import CommonModel
 # admin이 관리하는 공지사항 게시판 및 유저의 댓글 model
 
 class photo(CommonModel):
+
+    """Photos of a Notice Model Definition"""
+
     file=models.URLField(null=True, blank=True)
     notice=models.ForeignKey("notices.notie", on_delete=models.CASCADE)
 
 class notice(CommonModel):
+
+    """Notice Model Definition"""
+
+    
     title = models.CharField(max_length=255)
     content = models.TextField()
     owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 class comment(CommonModel):
+
+    """Comment Model Definition"""
+
     payload=models.TextField()
     user=models.ForeignKey("users.User",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} / {self.created_at}" 
--- reservations/models.py
+++ reservations/models.py
@@ -1,4 +1,29 @@
 from django.db import models
 from common.models import CommonModel
 
-#
+# 각 사업 기능에 대한 예약 Model
+
+class reservation(CommonModel):
+
+    """Reservation Model Definition"""
+
+    class BusinessChoices(models.TextChoices):
+        preschool = ("preschool", "Happy Preschool")
+        housing = ("housing", "Happy Housing")
+        farm = ("farm", "Happy farm")
+        salon = ("salon", "Happy Hair Salon")
+
+    business = models.CharField(max_length=255, choices=BusinessChoices.choices)
+    user = models.ForeignKey("users.user", on_delete=models.CASCADE)
+    check_in = models.DateField(
+        null=True,
+        blank=True,
+    )
+    check_out = models.DateField(
+        null=True,
+        blank=True,
+    )
+    guests = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.business} / booking for: {self.user}"
