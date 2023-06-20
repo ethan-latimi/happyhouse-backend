9ce644777465df09b4bc178855a94f8da47fe4a4
ethan <ethanminsu8969@gmail.com>
2023-06-20 17:28:35 +0900

    user model defined

--- users/models.py
+++ users/models.py
@@ -1,6 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# 웹사이트 회원 Model
+
 class User(AbstractUser):
-    pass
+    
+    """User Model Definition"""
+
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    avatar = models.URLField(null=True, blank=True)
+    is_resident = models.BooleanField(default=False)
+    is_parent= models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+    )
+    email = models.EmailField()
 
