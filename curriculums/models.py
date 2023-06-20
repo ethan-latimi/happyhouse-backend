a7b655bebb4db40f841b050cdd8e147b0f75d4a9
ethan <ethanminsu8969@gmail.com>
2023-06-20 16:52:16 +0900

    curriculum model defined

--- curriculums/models.py
+++ curriculums/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from common.models import CommonModel
+# 어린이집이 가지고 있는 학습 커리큘럼 model
 
-# Create your models here.
+class curriculum(CommonModel):
+
+    """Curriculum Model Definition"""
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    photo=models.URLField()
+
+    def __str__(self):
+        return self.title
