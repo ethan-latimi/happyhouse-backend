b42285ce41e4c8fc841bc323dc0887b9b3f4e313
ethan <ethanminsu8969@gmail.com>
2023-06-20 17:24:32 +0900

    teacher model defined

--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+# 선생님 정보 Model
+
+class teacher(CommonModel):
+
+    """Teacher Model Definition"""
+
+    nick_name=models.CharField(max_length=50)
+    bio=models.TextField()
+    photo=models.URLField(null=True, blank=True)
+
